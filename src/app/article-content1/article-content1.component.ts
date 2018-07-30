6dfd73ed62ef792854898c7a4f72f80130cbf947
Piyumi Rajapaksha <piyumi11@gmail.com>
2018-07-30 12:27:56 +0530

    new styles added

--- src/app/article-content1/article-content1.component.ts
+++ src/app/article-content1/article-content1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { escape } from 'querystring';
 
 @Component({
   selector: 'app-article-content1',
@@ -10,6 +11,15 @@ export class ArticleContent1Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  public sendMail() {
+    var link = "mailto:piyumi11@gmail.com"
+             "?cc=piyumi.rajapaksha@auxenta.com"
+             "&subject=" + escape("This is my subject")
+             "&body=" + escape("Test")
+
+    window.location.href = link;
   }
 
 }
