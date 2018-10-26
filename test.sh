706dc5e065559ffe1b7a9d7df239f28773a2b1b9
Piyumi Kaushalya <piyumik11@gmail.com>
2018-10-26 11:16:08 +0530

    Test-rm

--- test.sh
+++ test.sh
@@ -2,12 +2,10 @@
 echo "**** github page building process ****"
 
 echo "**** At Source Project Directory ****"
-git status
-git branch
 git add --all
 git commit -m $1
 git push
-
+echo "Source project changes committed to https://github.com/piyumi11/angularTest.git"
 # copy build files
 
 find "D:/dialog/projectname/dist/projectname" -maxdepth 1 -type f | xargs cp -t "D:/TestGit/piyumi11.github.io"
@@ -15,10 +13,10 @@ cp -r "D:\dialog\projectname\dist\projectname\assets" "D:/TestGit/piyumi11.githu
 
 echo "**** At Deployment Project Directory ****"
 cd "D:/TestGit/piyumi11.github.io"
-git status
 git add --all
-git status
-
+git commit -m $2
+git push
+echo "Built files changes committed to https://github.com/piyumi11/piyumi11.github.io.git"
 
 read -p "Press enter to continue"
 
