bc238bc03dd2a4657c26f71eaf121c8608e22e83
Piyumi Kaushalya <piyumik11@gmail.com>
2018-10-26 11:04:09 +0530

    Test

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-Echo **** github page building process ****
+echo "**** github page building process ****"
 
-echo **** At Source Project Directory ****
+echo "**** At Source Project Directory ****"
 git status
 git branch
 git add --all
@@ -10,9 +10,9 @@ git push
 
 # copy build files
 
-find "D:/dialog/projectname/dist/projectname" -maxdepth 5 -type f | xargs cp -t "D:/TestGit/piyumi11.github.io"
+find "D:/dialog/projectname/dist/projectname" -maxdepth 1 -type f | xargs cp -t "D:/TestGit/piyumi11.github.io"
 
-echo **** At Source Project Directory ****
+echo "**** At Source Project Directory ****"
 cd "D:/TestGit/piyumi11.github.io"
 git status
 
