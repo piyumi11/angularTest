2f66c93f36f29849bd930bc979c6093651f494bf
Piyumi Kaushalya <piyumik11@gmail.com>
2018-10-26 10:03:26 +0530

    %1

---
+++ test.sh
@@ -0,0 +1,16 @@
+
+echo test1
+
+# source project
+
+git status
+git branch
+git add --all
+git commit -m "%1"
+
+# copy build
+
+copy /y "D:\dialog\projectname\dist\projectname" "D:\TestGit\piyumi11.github.io"
+
+# deployment project
+
