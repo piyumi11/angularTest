0f8ef8d6b6cd87177328d70eb73b9de33f24cd76
piyumi11 <piyumik11@gmail.com>
2019-04-26 21:17:02 -0700

    php article and style changes

--- src/app/send-email/send-email.component.ts
+++ src/app/send-email/send-email.component.ts
@@ -18,7 +18,6 @@ export class SendEmailComponent implements OnInit {
 
   sendMail(response) {
     let link = 'mailto:piyumik11@gmail.com' +
-             '?cc=piyumi.rajapaksha@auxenta.com' +
              '&subject=User Feedback on piyumi11.github.io' +
              '&body=' + this.articleTitle +
              '%0D%0A' + ' Was this article helpful?' + response;
